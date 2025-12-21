bafae8fd18fee65942e160186aac75efd31842b4
s-elg <sumeyyeelgin@hotmail.com>
2025-12-21 23:51:07 +0300

    login sayfası frontend yapıldı. projeyi kaldırınca direkt login'e gidiyor

--- BitirmeProjesi.Web/Controllers/AccountController.cs
+++ BitirmeProjesi.Web/Controllers/AccountController.cs
@@ -1,6 +1,24 @@
-﻿namespace BitirmeProjesi.Web.Controllers
+﻿using BitirmeProjesi.Web.Models.Account;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BitirmeProjesi.Web.Controllers
 {
-    public class AccountController
+    public class AccountController : Controller
     {
+        public IActionResult Login()
+        {
+            return View(new LoginViewModel());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            // TODO: Auth işlemleri (Identity vs)
+            // model.Role'a göre yönlendirme yaparsın.
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
--- BitirmeProjesi.Web/Models/Account/LoginViewModel.cs
+++ BitirmeProjesi.Web/Models/Account/LoginViewModel.cs
@@ -1,6 +1,19 @@
-﻿namespace BitirmeProjesi.Web.Models.Account
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BitirmeProjesi.Web.Models.Account
 {
     public class LoginViewModel
     {
+        [Required(ErrorMessage = "Rol seçiniz.")]
+        public string Role { get; set; } = "Student"; // Student | Advisor
+
+        [Required(ErrorMessage = "Kullanıcı adı zorunlu.")]
+        public string Username { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Parola zorunlu.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+
+        public bool RememberMe { get; set; } = true;
     }
 }
