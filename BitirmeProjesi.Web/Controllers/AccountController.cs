a20ae0b162b947a05e3abd1d410e8673f9bc928e
Ervabayburtlu <130878119+Ervabayburtlu@users.noreply.github.com>
2025-12-21 23:24:10 +0300

    Add login and register actions to AccountController

--- BitirmeProjesi.Web/Controllers/AccountController.cs
+++ BitirmeProjesi.Web/Controllers/AccountController.cs
@@ -2,5 +2,31 @@
 {
     public class AccountController
     {
+         public IActionResult Login()
+ {
+     return View();
+ }
+
+ [HttpPost]
+ public IActionResult Register(string name, string studentNumber, string password)
+ {
+     // TODO: _context.Students.Add(new Student { ... });
+     // _context.SaveChanges();
+     return RedirectToAction("Login");
+ }
+
+ [HttpPost]
+ public IActionResult Login(string studentNumber, string password)
+ {
+ TODO:
+     ViewBag.Error = "Okul numarası veya şifre hatalı!";
+     return View();
+ }
+
+ public IActionResult Logout()
+ {
+     HttpContext.Session.Clear(); // Oturumu temizle
+     return RedirectToAction("Login");
+ }
     }
 }
