91ecceca4355d1618fa956a034516475b947f759
Ervabayburtlu <130878119+Ervabayburtlu@users.noreply.github.com>
2025-12-21 23:24:51 +0300

    Add StudentExcuseController with basic actions

--- BitirmeProjesi.Web/Controllers/StudentExcuseController.cs
+++ BitirmeProjesi.Web/Controllers/StudentExcuseController.cs
@@ -2,5 +2,22 @@
 {
     public class StudentExcuseController
     {
+        public class ExcusesRequest : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+    public IActionResult NewRequest()
+    {
+        return View();
+    }
+
+    // 3. Detayları Görüntüle ("Detayları Görüntüle" Linki)
+    public IActionResult Detail(int id)
+    {
+        // id'ye göre mazeretin tüm detaylarını (reddedilme nedeni, eklenen belgeler vb.) getirir.
+        return View();
+    }
     }
 }
